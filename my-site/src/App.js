e3e64b9f2d22098734e8d7e3f27e68385465c07b
Faraz Oman <farazoman@system76-pc.localdomain>
2023-08-13 20:04:41 -0600

    Keep info upto date. Modified opening linked-in in a new tab so it doesn't replace the existing window.

--- my-site/src/App.js
+++ my-site/src/App.js
@@ -2,9 +2,10 @@ import './App.css';
 import Terminal from 'terminal-in-react';
 
 let showAboutMsg = () => `Hello, welcome to my site, my name is Faraz and I graduated from McGill University with a degree in Software Engineering. 
-I'm currently working on the Scheduling team at Ceridian, an HCM company and have worked there previously as a Software Developer in Test. 
 
-In my free time I enjoy to read, listen to music, cook and bake, go out for bike rides, and spend time in nature.
+I'm currently working as a Cloud Engineer on the Core Safety Team at Veeva, where my department deals with pharma-covigilance. Previously, I worked for Ceridian, an HCM company with the positions of Software Developer in Test, and Software Developer.
+
+In my free time I enjoy to read, listen to music, cook and bake, go out for bike rides, and spend time in nature. Tambien me encanta echaar los mejores tacos del mundo en cdmx, y discrubiendo nuevas cosas en la ciudad cada dia.
 
 If you want to get in touch with me, you can reach me at farazoman@gmail.com or contact me on my LinkedIn
 
@@ -26,9 +27,12 @@ function App() {
         barColor='black'
         style={{ fontWeight: "bold", fontSize: "1em" }}
         commands={{
-          linkedin: () => window.location.replace('https://ca.linkedin.com/in/farazoman'),
+          linkedin: () => {
+            window.open('https://ca.linkedin.com/in/farazoman', "_blank")
+            
+          },
           aboutme: showAboutMsg,
-          skills: () => 'C#, Selenium, SQL, SpecFlow, Powershell, Javascript, React, Adobe Creative Suite (Illustrator, InDesign, Photoshop)',
+          skills: () => 'Python, Java, AWS, Ansible, Mircoservices, C#, Selenium, SQL, SpecFlow, Powershell, Javascript, React, Adobe Creative Suite (Illustrator, InDesign, Photoshop)',
           contact: () => 'email: farazoman@gmail.com'
         }}
         descriptions={{
@@ -38,7 +42,7 @@ function App() {
           contact: 'get in touch!'
         }}
         startState='maximised'
-        msg="Welcome to my site, I'm Faraz and I work as a Software Developer. Type 'help' to see all the available commands. Enjoy!"
+        msg="Welcome to my site, I'm Faraz and I work as a Cloud Software Engineer. Type 'help' to see all the available commands. Enjoy!"
       />
     </div>
   );
